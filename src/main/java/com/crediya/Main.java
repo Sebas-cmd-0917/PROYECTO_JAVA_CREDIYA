fe2a6401d227ff93869e51e417aa5c3bf4d9656c
Marcela Albarracin <lmalbarracin57@gmail.com>
2025-12-15 22:40:21 -0500

    Style: :lipstick: Menus emojis

--- src/main/java/com/crediya/Main.java
+++ src/main/java/com/crediya/Main.java
@@ -8,31 +8,34 @@ import com.crediya.ui.MenuPrincipal;
 
 public class Main {
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         GestorEmpleadoService authService = new GestorEmpleadoService();
-        
-        while (true) {
-            System.out.println("========================================");
-            System.out.println("==== BIENVENIDO A CREDIYA MAYSE ========");
-            System.out.println("========================================");
 
-            System.out.println("--------- INICIO DE SESIÓN ------------");
-            System.out.println("Por favor, ingrese sus credenciales.");
-            System.out.println("---------------------------------------");
-            System.out.print("Correo: ");
+        while (true) {
+            System.out.println("\n");
+            System.out.println("╔══════════════════════════════════════════════╗");
+            System.out.println("║       🏦  BIENVENIDO A CREDIYA MAYSE 🏦      ║");
+            System.out.println("╠══════════════════════════════════════════════╣");
+            System.out.println("║                                              ║");
+            System.out.println("║           🔐  INICIO DE SESIÓN  🔐           ║");
+            System.out.println("║                                              ║");
+            System.out.println("╚══════════════════════════════════════════════╝");
+            System.out.println("   Por favor, identifíquese para continuar.");
+            System.out.println("────────────────────────────────────────────────");
+            System.out.print("👉 Ingrese su Correo: ");
             String correo = scanner.nextLine();
-            
+
             try {
                 // 1. Esto busca el empleado en la BD. Si existe, te devuelve el objeto lleno.
                 Empleado usuarioEncontrado = authService.iniciarSesion(correo);
-                
+
                 // 2. ¡AQUÍ ESTÁ LA CLAVE! Pasas el objeto real, NO null.
                 MenuPrincipal menu = new MenuPrincipal(usuarioEncontrado);
                 menu.mostrarMenu();
-                
+
                 // Si sale del menú, rompe el ciclo
                 break;
-                
+
             } catch (Exception e) {
                 System.out.println("❌ " + e.getMessage());
             }
